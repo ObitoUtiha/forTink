1779ce7fe9fd074903b105e879bb5bbdf64ed975
ObitoUtiha <82911832+ObitoUtiha@users.noreply.github.com>
2024-09-16 21:58:39 +0300

    Add proj

---
+++ TinkSixSolution/TinkSixSolution/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinkSixSolution
+{
+    internal class Program
+    {
+        class Process
+        {
+            public int processTime { get; set; }
+            public int[] processRelative { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            int numberProc = Convert.ToInt32(Console.ReadLine());
+
+
+            List<Process> process = new List<Process>();
+            for (int i = 0; i < numberProc; i++)
+            {
+                Console.WriteLine("Введите колчество секунд и зависимые процессы");
+                string infoProc = Console.ReadLine();
+                int processTime = Convert.ToInt32(infoProc.Trim().Split(' ')[0]);
+                int[] processRelative = new int[infoProc.Trim().Split(' ').Length - 1];
+                for (int j = 0; j < infoProc.Trim().Split(' ').Length - 1; j++)
+                {
+                    processRelative[j] = Convert.ToInt32(infoProc.Trim().Split(' ')[j + 1]);
+                }
+                process.Add(new Process()
+                {
+                    processTime = processTime,
+                    processRelative = processRelative
+                });
+            }
+            int? result = 0;
+            bool check = true;
+            while (check)
+            {
+                bool[] chekers = new bool[numberProc];
+                int proccesNumber = 0;
+                foreach (Process proc in process)
+                {
+                    if (proc.processTime <= 0)
+                    {
+                        chekers[proccesNumber] = true;
+                        proccesNumber++;
+                        continue;
+                    }
+                    if (proc.processRelative == null)
+                    {
+                        proc.processTime -= 1;
+                    }
+                    bool inProcessChecker = true;
+                    for (int i = 0; i < proc.processRelative.Length; i++)
+                    {
+                        int relativeNum = proc.processRelative[i];
+                        if (process[relativeNum - 1].processTime != 0)
+                        {
+                            inProcessChecker = false;
+                            break;
+                        }
+                    }
+                    if (inProcessChecker)
+                        proc.processTime -= 1;
+
+                    proccesNumber++;
+                }
+
+                if (chekers.Contains(false))
+                {
+                    result++;
+                }
+                else
+                {
+                    check = false;
+                    break;
+                }
+
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TinkSixSolution/TinkSixSolution/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TinkSixSolution")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TinkSixSolution")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2e7bbb14-c769-47fa-956c-6d1b02297562")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
